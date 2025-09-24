fc4fa8e32c6e9ecc63221471365f5541f3851ec0
Lazy22zz <claudeleave@gmail.com>
2025-09-23 19:42:06 -0700

    add attribute health, mana

--- Source/Crunch/Crunch.Build.cs
+++ Source/Crunch/Crunch.Build.cs
@@ -8,9 +8,10 @@ public class Crunch : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities",
+    "GameplayTags", "GameplayTasks"  });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags"});
+        PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });

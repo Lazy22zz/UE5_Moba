274986dfa906c19c4a7d75235d2e945e3fd5ddbe
Lazy22zz <claudeleave@gmail.com>
2025-10-13 20:37:04 -0700

    Add the Animation Montage Play for the Combo Ability

--- Source/Crunch/Private/GAS/GA_Combo.h
+++ Source/Crunch/Private/GAS/GA_Combo.h
@@ -16,4 +16,8 @@ class UGA_Combo : public UCGameplayAbility
 	
 public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UAnimMontage* ComboMontage;
 };
